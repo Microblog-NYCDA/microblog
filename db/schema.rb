be866d035b8f547e17260a7c134224accbafc4a9
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-05-18 15:07:03 -0400

    initial, added .gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518150430) do
+ActiveRecord::Schema.define(version: 20170518183314) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -25,11 +25,17 @@ ActiveRecord::Schema.define(version: 20170518150430) do
     t.integer "user_id"
   end
 
+  create_table "requests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "requester_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
     t.string "password"
     t.integer "user_id"
+    t.integer "friend_id"
   end
 
 end
