7b37d49b36d2ed379d6b3814112c8caccdb9a134
Troy Walley <walley.troy@yahoo.com>
2017-05-18 15:53:06 -0400

    added schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518183314) do
+ActiveRecord::Schema.define(version: 20170518192326) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.integer "user_id"
     t.integer "post_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
   end
 
   create_table "posts", force: :cascade do |t|
